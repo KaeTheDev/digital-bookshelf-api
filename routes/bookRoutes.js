ec58943c1103ad11b98ce9ae9370438ee7d26335
KaeTheDev <kaethedev@gmail.com>
2026-01-13 17:46:23 -0500

    Added get all model route

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -11,3 +11,14 @@ router.post('/', async(req, res) => {
         res.status(400).json({ error: err.message });
     }
 });
+
+
+// GET all books
+router.get('/', async(req, res) => {
+    try {
+        const books = await Book.find();
+        res.json(books);
+    } catch(err) {
+        res.status(500).json({ error: err.message });
+    }
+});
