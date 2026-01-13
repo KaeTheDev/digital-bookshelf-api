93735b1724c5dabc0fc165ac196bae104e01fac3
KaeTheDev <kaethedev@gmail.com>
2026-01-13 17:47:09 -0500

    added update book model route

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -34,3 +34,14 @@ router.get('/:id', async(req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
+// UPDATE a book
+router.put('/:id', async(req, res) => {
+    try {
+        const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!updatedBook) return res.status(404).json({ error: 'Book not found' });
+        res.json(updatedBook);
+    } catch(err) {
+        res.status(500).json({ error: err.message });
+    }
+});
