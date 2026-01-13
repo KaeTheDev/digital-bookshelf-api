adacdc4b8175903a4617d4d7c07adf12689720d0
KaeTheDev <kaethedev@gmail.com>
2026-01-13 17:46:48 -0500

    Added get one book model route

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -22,3 +22,15 @@ router.get('/', async(req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
+
+// GET one book
+router.get('/:id', async(req, res) => {
+    try{
+        const book = await Book.findById(req.params.id);
+        if(!book) return res.status(404).json({ error: 'Book not found' });
+        res.json(book);
+    } catch(err) {
+        res.status(500).json({ error: err.message });
+    }
+});
